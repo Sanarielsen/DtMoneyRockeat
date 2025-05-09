efd912c46dccbd3b18ec61da5247b89ea9e83cfe
Sanarielsen <sanarielsen.dev@gmail.com>
2025-05-09 15:27:07 -0300

    feat(DMR-02): create context and configure axios
    Create the first Context API to manage Transactions in a application and
    all initial configuration to use JSON Server as a Back-End If you have
    but in this moment, we need to simulate as a Back-End to prepare a
    communication between Front-End and Back-End.
    
    Install and configure things like React Hook Forms, create an example
    hook and how can I use formatter when you need a format date and price.
    
    Install and configure axios swiching of fetch, because it can make the
    code more organized and lint to apply specific formats can be help to
    organize code in general.
    
    Resolves: DMR-02-01, DMR-02-02, DMR-02-03

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,11 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
+
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
@@ -12,7 +16,7 @@ export function Summary() {
 
         </header>
         
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -22,7 +26,7 @@ export function Summary() {
 
         </header>
         
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -32,7 +36,7 @@ export function Summary() {
 
         </header>
         
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
