78bbc6dfde23b40ffaea24ce4298fd664bfda2e8
Sanarielsen <sanarielsen.dev@gmail.com>
2025-05-09 15:27:07 -0300

    feat(DMR-2): create context and initial configuration
    Create the first Context API to manage Transactions in a application and
    all initial configuration to use JSON Server as a Back-End If you have
    but in this moment, we need to simulate as a Back-End to prepare a
    communication between Front-End and Back-End.
    
    Resolves: DMR-2-1

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,31 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
+
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price;
+      acc.total += transaction.price;
+
+      return acc;
+    }
+
+    acc.outcome += transaction.price;
+    acc.total -= transaction.price;
+
+    return acc;
+  },  
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  );
 
   return (
     <SummaryContainer>
@@ -12,7 +36,7 @@ export function Summary() {
 
         </header>
         
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -22,7 +46,7 @@ export function Summary() {
 
         </header>
         
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -32,7 +56,7 @@ export function Summary() {
 
         </header>
         
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
