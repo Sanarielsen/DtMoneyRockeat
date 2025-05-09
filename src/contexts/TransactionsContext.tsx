210cdf5d463358b0db43134ee5066d6297b5aae0
Sanarielsen <sanarielsen.dev@gmail.com>
2025-05-09 15:27:07 -0300

    feat(DMR-02): create context and configure axios
    Create the first Context API to manage Transactions in a application and
    all initial configuration to use JSON Server as a Back-End If you have
    but in this moment, we need to simulate as a Back-End to prepare a
    communication between Front-End and Back-End.
    
    Install and configure things like React Hook Forms, create an example
    hook and how can I use formatter when you need a format date and price.
    
    Install and configure axios swiching of fetch, because it can make the
    code more organized and lint to apply specific formats can be help to
    organize code in general.
    
    Resolves: DMR-02-01, DMR-02-02, DMR-02-03

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,79 @@
+import { createContext, useEffect, useState, type ReactNode } from "react";
+import { api } from "../lib/axios";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface CreateTransactionInput {
+  description: string;
+  price: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
+
+interface TransactionContextType {
+
+  transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
+}
+
+interface TransactionsProviderProps {
+
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext<TransactionContextType>({} as TransactionContextType);
+
+export function TransactionsProvider({children}: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function fetchTransactions(query?: string) {
+    const response = await api.get('/transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query
+      }
+    })
+    
+    setTransactions(response.data);
+  }
+
+  async function createTransaction(data: CreateTransactionInput) {
+
+    const { description, price, category, type } = data
+
+    //O id cria automaticamente no SQL:Server
+    const response = await api.post('/transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date()
+    })
+
+    setTransactions( state => [response.data, ...state]);
+  }
+  
+  useEffect(() => {
+  
+    fetchTransactions();
+  },[])
+
+  return (
+    <TransactionsContext.Provider value={{ 
+      transactions, 
+      fetchTransactions,
+      createTransaction
+    }}> 
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
