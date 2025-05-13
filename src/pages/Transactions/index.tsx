29f9a9924d52ff7188c840949b07efe0c06e993d
Sanarielsen <sanarielsen.dev@gmail.com>
2025-05-13 13:24:49 -0300

    feat(DMR-02): install and configure things to use in a forms
    Install and configure things like React Hook Forms, create an example
    hook and how can I use formatter when you need a format date and price.
    
    Resolves: DMR-02-02

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,18 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./style";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
-
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
@@ -34,11 +26,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
